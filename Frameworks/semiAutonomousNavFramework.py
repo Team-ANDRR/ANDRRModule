7380aa2d073baf2d8019176b6206ac8ad28e95da
nate-roy <110704639+nate-roy@users.noreply.github.com>
2025-01-27 09:58:13 -0500

    Update semiAutonomousNavFramework.py

--- Frameworks/semiAutonomousNavFramework.py
+++ Frameworks/semiAutonomousNavFramework.py
@@ -12,6 +12,7 @@ import csv
 import serial
 import detector
 from multiprocessing import Process,Queue
+from pymavlink import mavutil
 
 
 class ANDRRFramework:
@@ -26,7 +27,7 @@ class ANDRRFramework:
         self.showImage=True #If true, images will be displayed on screen
         self.imW=800 #Image resolution width
         self.imH=450 #Image resolution height
-        self.serOut='/dev/ttyACM0' #Serial port for outputting processed data
+        self.serOut='/dev/ttyACM2' #Serial port for outputting processed data
         self.serIn='/dev/ttyACM1' #Serial port for MAVSDK connection
 
         #THE FOLLOWING DO NOT NEED TO BE EDITED
@@ -50,7 +51,7 @@ class ANDRRFramework:
 
         #Initialize serial output
         try:
-            self.serOut=serial.Serial(self.serPort, 115200, timeout=10) #Connects to the Arduino over serial
+            self.serOut=serial.Serial(self.serOut, 115200, timeout=10) #Connects to the Arduino over serial
             time.sleep(2)
             self.serOut.reset_input_buffer()
             self.serOut.write(b"start\n")
@@ -65,7 +66,7 @@ class ANDRRFramework:
             self.serIn = mavutil.mavlink_connection(self.serIn, baud=921600) # Adjust the port and baud rate if necessary
             self.serIn.wait_heartbeat()
             print("CONNECTED TO MAVLINK")
-            self.serIn.mav.request_data_stream_send(master.target_system, master.target_component, mavutil.mavlink.MAV_DATA_STREAM_ALL, 5, 1) # Request all data streams at 5Hz
+            self.serIn.mav.request_data_stream_send(self.serIn.target_system, self.serIn.target_component, mavutil.mavlink.MAV_DATA_STREAM_ALL, 10, 1) # Request all data streams at 10Hz
         except:
             self.serIn=None
             print("ERROR: FAILED TO CONNECT TO MAVLINK")
@@ -87,17 +88,21 @@ class ANDRRFramework:
     def dataUpdate(self, queueIn, queueOut): #Sends/receives data to/from the arduino
         loopTime=0
         while True:
-            sendString=str(queueIn.get())+'\n'
+            sendString=''
+            if not queueIn.empty():
+                sendString=str(queueIn.get())+'\n'
             #Send over serial
             if self.serOut!=None and len(sendString)>0:
                 self.serOut.reset_input_buffer()
                 self.serOut.write(bytes(sendString, 'utf-8'))
             if self.serIn!=None:
-                msg = master.recv_msg()
-                if msg.get_type()=='GLOBAL_POSITION_INT':
-                    msg.lat=msg.lat/1e7
-                    msg.lon=msg.lon=1e7
-                    queueOut.put(msg)
+                msg = self.serIn.recv_msg()
+                print(msg)
+                if msg!=None:
+                    if msg.get_type()=='GLOBAL_POSITION_INT':
+                        msg.lat=msg.lat/1e7
+                        msg.lon=msg.lon/1e7
+                        queueOut.put(msg)
         pass
 
     def processImage(self, dataOutQueue, dataInQueue, imageOutQueue): #Run an image through the CV program, add the relevant data, save the image
@@ -107,6 +112,10 @@ class ANDRRFramework:
         ret = cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
         ret = cap.set(3,self.imW)
         ret = cap.set(4,self.imH)
+
+        txtfile=self.folderName+"runtimeData.txt"
+        with open(txtfile, 'w',newline='') as f:
+            f.write('0\n')
 
         #Create a data boarder
         labelinit = np.zeros((30,self.imW,3), np.uint8)
@@ -137,7 +146,7 @@ class ANDRRFramework:
                 minutes=int((captureTime-seconds)/60)
                 timeStamp = "Time: " + str(minutes) + ":" + str(seconds).zfill(2)
 
-                if self.serIn!=None:
+                if self.serIn!=None and not dataInQueue.empty():
                     GPS=dataInQueue.get()
                     GPStext="GPS: " + str(GPS.lat) + "," + str(GPS.lon)
                 else:
@@ -168,7 +177,13 @@ class ANDRRFramework:
                 cv2.imshow('radioImage', frame)
                 if cv2.waitKey(1) == ord('q'):
                     break 
+            #loopFPS=1/(time.time()-tic1)
+            #txtfile=self.folderName+"runtimeData.txt"
+            #with open(txtfile, 'a',newline='') as f:
+                #f.write(str(loopFPS)+'\n')
 
+            #if ID>200:
+                #quit()
 
     def storeImage(self, queue):
         ID=0
@@ -202,15 +217,17 @@ time.sleep(1)
 #Setup display window
 cv2.namedWindow('radioImage', cv2.WINDOW_NORMAL)
 
-#Create queues for passing data between parallel processes
+
 dataOutQueue=Queue()
 dataInQueue=Queue()
 imageOutQueue=Queue()
+
 dataProcess=Process(target=ANDRR.dataUpdate, args=(dataOutQueue,dataInQueue, ))
 saveImageProcess=Process(target=ANDRR.storeImage, args=(imageOutQueue, ))
 
 dataProcess.start()
 saveImageProcess.start()
+
 
 ANDRR.processImage(dataOutQueue,dataInQueue,imageOutQueue)
 
